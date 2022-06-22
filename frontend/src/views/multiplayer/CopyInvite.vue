ea2412d97ef2102ac87e301b9eef409a124f5845
Florin Barbisch <florin.barbisch@gmail.com>
2022-06-22 11:33:50 +0200

    Fixed: copy invite linked to an invalid url

--- frontend/src/views/multiplayer/CopyInvite.vue
+++ frontend/src/views/multiplayer/CopyInvite.vue
@@ -5,17 +5,18 @@
         class="col-sm-10 col-md-8 col-lg-6 col-xl-6 offset-sm-1 offset-md-2 offset-lg-3 offset-xl-3 text-center"
       >
         <h3 class="text-center">
-          Copy your Invite Link and send it to your Opponent
+          Copy your
+          <a :href="'/multiplayer/play/' + currentMultiplayerGame._id">
+            <span style="text-decoration: underline;">
+              Invite Link
+            </span>
+          </a>
+          and send it to your Opponent
         </h3>
-        <a :href="API_URL + '/multiplayer/play/' + currentMultiplayerGame._id">
-          <span style="text-decoration: underline;">{{
-            API_URL + "/multiplayer/play/" + currentMultiplayerGame._id
-          }}</span></a
-        >
         <a
           class="btn btn-primary btn-block text-center border rounded-pill"
           role="button"
-          :href="API_URL + '/multiplayer/play/' + currentMultiplayerGame._id"
+          :href="'/multiplayer/play/' + currentMultiplayerGame._id"
           >Continue to game!</a
         >
       </div>
@@ -27,15 +28,13 @@
 import { mapGetters } from "vuex";
 import store from "@/store";
 import { FETCH_MULTIPLAYER_GAME } from "@/store/actions.type";
-import { API_URL } from "@/common/config";
 
 export default {
   name: "CopyLink",
   computed: {
     ...mapGetters({
       currentMultiplayerGame: "currentMultiplayerGame"
-    }),
-    API_URL
+    })
   },
   beforeRouteEnter(to, from, next) {
     Promise.all([
