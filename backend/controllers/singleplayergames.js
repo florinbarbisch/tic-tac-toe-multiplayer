072698093435c8cd5956c8fce4e4d96777637a2f
Florin Barbisch <florin.barbisch@gmail.com>
2022-06-22 11:04:52 +0200

    Moved: single player code to controller

---
+++ backend/controllers/singleplayergames.js
@@ -0,0 +1,71 @@
+var mongoose = require('mongoose');
+var SingleplayerGame = mongoose.model('SingleplayerGame');
+var User = mongoose.model('User');
+var auth = require('../auth');
+
+exports.getSingleplayerGamesController = function(req, res, next) {
+  User.findById(req.payload.id).then(function(user){
+    var query = { player: user };
+    if(req.query.difficulty) {
+      query.difficulty = req.query.difficulty;
+    }
+    if(req.query.status == 'Finished') {
+      query.status = { $ne: 'Ongoing'};
+    } else if (req.query.status) {
+      query.status = req.query.status;
+    }
+    return Promise.all([
+      SingleplayerGame.find(query)
+        .populate('player')
+        .sort({createdAt: 'desc'})
+        .exec(),
+        User.findById(req.payload.id),
+    ]).then(function(results){
+      var games = results[0];
+      var user = results[1];
+
+      return res.json({singleplayerGames: games.map(game => game.toJSONFor(user))});
+    }).catch(next);
+  }).catch(next);
+}
+
+exports.postSingleplayerGameController = function(req, res, next) {
+  User.findById(req.payload.id).then(function(user){
+    if (!user) { return res.sendStatus(401); }
+    if (!req.body.difficulty) { return res.status(422).send({difficulty: "can't be blank"}); }
+
+    var game = new SingleplayerGame();
+
+    game.player = user;
+    game.difficulty = req.body.difficulty;
+    req.payload ? User.findById(req.payload.id) : null;
+
+    return game.save().then(function(){
+      console.log(game.player);
+      return res.json({SingleplayerGame: game.toJSONFor(user)});
+    });
+  }).catch(next);
+}
+
+
+exports.getSingleplayerGameController = function(req, res, next) {
+  return Promise.all([
+    SingleplayerGame.findById({_id: req.game._id})
+      .exec(),
+  ]).then(function(results){
+    return res.json({SingleplayerGame: results[0]});
+  }).catch(next);
+}
+
+exports.moveSingleplayerGameController = function(req, res, next){
+  User.findById(req.payload.id).then(function(user){
+    if (!user) { return res.sendStatus(401); }
+    if (!req.body.cell && req.body.cell !== 0) { return res.status(422).send({cell: "can't be blank"}); }
+    if (req.game.getBoard()[req.body.cell]) { return res.status(422).send({field: "already set"}); }
+    if (req.game.getWinner()) { return res.status(422).send({game: "already finished"}); }
+
+    return req.game.move(user, req.body.cell).then(function(){
+      return res.json({SingleplayerGame: req.game.toJSONFor(user)});
+    });
+  }).catch(next);
+}
--- backend/routes/api/singleplayergames.js
+++ backend/routes/api/singleplayergames.js
@@ -1,7 +1,7 @@
 var router = require('express').Router();
 var mongoose = require('mongoose');
+var singleplayergamesController = require('../../controllers/singleplayergames');
 var SingleplayerGame = mongoose.model('SingleplayerGame');
-var User = mongoose.model('User');
 var auth = require('../auth');
 
 // Preload game on routes with ':game'
@@ -16,71 +16,12 @@ router.param('game', function(req, res, next, game) {
   }).catch(next);
 });
 
-router.get('/', auth.required, function(req, res, next) {
-  User.findById(req.payload.id).then(function(user){
-    var query = { player: user };
-    if(req.query.difficulty) {
-      query.difficulty = req.query.difficulty;
-    }
-    if(req.query.status == 'Finished') {
-      query.status = { $ne: 'Ongoing'};
-    } else if (req.query.status) {
-      query.status = req.query.status;
-    }
-    return Promise.all([
-      SingleplayerGame.find(query)
-        .populate('player')
-        .sort({createdAt: 'desc'})
-        .exec(),
-        User.findById(req.payload.id),
-    ]).then(function(results){
-      var games = results[0];
-      var user = results[1];
+router.get('/', auth.required, singleplayergamesController.getSingleplayerGamesController);
 
-      return res.json({singleplayerGames: games.map(game => game.toJSONFor(user))});
-    }).catch(next);
-  }).catch(next);
-});
+router.post('/', auth.required, singleplayergamesController.postSingleplayerGameController);
 
-router.post('/', auth.required, function(req, res, next) {
-  User.findById(req.payload.id).then(function(user){
-    if (!user) { return res.sendStatus(401); }
-    if (!req.body.difficulty) { return res.status(422).send({difficulty: "can't be blank"}); }
+router.get('/:game', auth.required, singleplayergamesController.getSingleplayerGameController);
 
-    var game = new SingleplayerGame();
-
-    game.player = user;
-    game.difficulty = req.body.difficulty;
-    req.payload ? User.findById(req.payload.id) : null;
-
-    return game.save().then(function(){
-      console.log(game.player);
-      return res.json({SingleplayerGame: game.toJSONFor(user)});
-    });
-  }).catch(next);
-});
-
-router.get('/:game', auth.required, function(req, res, next) {
-  return Promise.all([
-    SingleplayerGame.findById({_id: req.game._id})
-      .exec(),
-  ]).then(function(results){
-    return res.json({SingleplayerGame: results[0]});
-  }).catch(next);
-});
-
-router.post('/:game/move', auth.required, function(req, res, next){
-  User.findById(req.payload.id).then(function(user){
-    if (!user) { return res.sendStatus(401); }
-    if (!req.body.cell && req.body.cell !== 0) { return res.status(422).send({cell: "can't be blank"}); }
-    if (req.game.getBoard()[req.body.cell]) { return res.status(422).send({field: "already set"}); }
-    if (req.game.getWinner()) { return res.status(422).send({game: "already finished"}); }
-
-    return req.game.move(user, req.body.cell).then(function(){
-      return res.json({SingleplayerGame: req.game.toJSONFor(user)});
-    });
-  }).catch(next);
-});
-
+router.post('/:game/move', auth.required, singleplayergamesController.moveSingleplayerGameController);
 
 module.exports = router;
